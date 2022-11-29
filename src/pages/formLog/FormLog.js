7d8454ad14814f6976ce5a2efedfb652225d9c85
unknown <erkubekt@gmail.com>
2022-11-29 21:11:19 +0600

    updated

---
+++ src/pages/formLog/FormLog.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './FormLog.module.css'
+
+function FormLog() {
+  return (
+    <div>FormLog</div>
+  )
+}
+
+export default FormLog
---
+++ src/pages/formSign/FormSign.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './FormSign.module.css'
+
+function FormSign() {
+  return (
+    <div>FormSign</div>
+  )
+}
+
+export default FormSign
