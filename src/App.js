154f8847090c4166753fea170c35d5570ddf97bb
unknown <erkubekt@gmail.com>
2022-12-05 20:18:40 +0600

    updated 5.12

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter,Route,Routes, } from 'react-router-dom';
 
 import Main from './Main';
+import IndoorPage from './pages/catologPages/indoorPage/IndoorPage';
+import MainCatalog from './pages/catologPages/mainCatalog/MainCatalog';
+import FormLog from './pages/formPages/formLog/FormLog';
+import FormSignUp from './pages/formPages/formSignUp/FormSignUp';
+import MainPage from './pages/mainPage/MainPage';
 
 function App() {
 
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='MainPage' element={<MainPage/>}/>
+          <Route path='Log' element={<FormLog/>}/>
+          <Route path='SignUp' element={<FormSignUp/>}/>
+          <Route path='SignUp' element={<FormSignUp/>}/>
+          <Route path='MainCatalog' element={<MainCatalog/>}/>
+          <Route path='IndoorPage' element={<IndoorPage/>}/>
+        </Routes>
+      </BrowserRouter>
       <Main/>
     </div>
   );
--- src/Main.js
+++ src/Main.js
@@ -7,6 +7,7 @@ import './Main.css'
 function Main() {
   return (
     <div >
+      
     </div>
   )
 }
