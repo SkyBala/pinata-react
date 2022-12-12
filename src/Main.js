d9d24118c2a1a79e62b4b65ce1e2651f7c1322f6
tokonov <tokonovabai28@gmail.com>
2022-12-13 03:12:21 +0600

    Header_Abai

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Main.css'
-
+import Header from "./components/header/Header";
 
 
 
 function Main() {
   return (
     <div >
-      
+      <Header/>
     </div>
   )
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from './Header.module.css'
+import './Header.css'
+import HeaderLogo from './img/logo.svg'
+import HeaderSearch from './img/search.svg'
+import HeaderBasket from './img/basket.svg'
+import HeaderUser from './img/user.svg'
+import {NavLink } from "react-router-dom";
+import HeaderModal from "./Components/HeaderModal";
+
+
 
 function Header() {
+
+const [showModal, setShowModal] = useState(false)
   return (
-    <div>Header</div>
+    <header className={classes.header}
+        onMouseEnter={ () =>{
+            setShowModal(true)
+        }
+        }onMouseLeave={() => {
+            setShowModal(false);
+        }}
+
+    >
+      <div className="container">
+          <div className={classes.header_inner}>
+              <div className={classes.header_logo}>
+                  <a href=""><img src={HeaderLogo} alt=""/></a>
+              </div>
+              <div className={classes.header_nav}>
+                  <ul className={classes.header_nav_ul}>
+                      <li><NavLink >PLANTS</NavLink></li>
+                      <li><NavLink >CARE TOOLS </NavLink></li>
+                      <li><NavLink >GIFTS</NavLink></li>
+                      <li><NavLink >LEARN</NavLink></li>
+                  </ul>
+              </div>
+              <div className={classes.header_nav_icons}>
+                  <ul className={classes.header_nav_icon_ul}>
+                      <li><a href=""><img src={HeaderSearch} alt=""/></a></li>
+                      <li><a href=""><img src={HeaderBasket} alt=""/></a></li>
+                      <li><a href=""><img src={HeaderUser} alt=""/></a></li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+        {
+            showModal &&
+            <HeaderModal />
+        }
+
+    </header>
+
   )
 }
 
