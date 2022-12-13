d4b61edef637cf7577c11c0f9c1d91bb4736d00b
AhegaoXXX <akaredover@gmail.com>
2022-12-14 02:07:05 +0600

    Final_project

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import './Main.css'
+import MainCatalog from './pages/catologPages/mainCatalog/MainCatalog'
+import IndoorInner from './pages/catologPages/indoorInner/IndoorInner';
+import MobileCatalog from "./pages/catologPages/mobileCatalog/MobileCatalog";
+import News from "./pages/catologPages/mobileCatalog/news/News";
+import MobileMainCatalog from './pages/catologPages/mobileMainCatalog/MobileMainCatalog';
 
 
 
 
 function Main() {
+
   return (
     <div >
-      
+      {/* <IndoorInner/> */}
+      {/* <MainCatalog/> */}
+      {/* <MobileMainCatalog/> */}
+      {/* <MobileCatalog/> */}
+      {/* <News/> */}
+      {/* <Catalog/> */}
     </div>
   )
 }
