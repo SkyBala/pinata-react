d83c25c2e41d0a6aa9f84007ab7cb6e20fe0e780
unknown <erkubekt@gmail.com>
2022-12-14 16:33:53 +0600

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import FirstSlider from './pages/mainPage/mainPageComponents/sliders/firstSlider/FirstSlider';
+
 
 
 
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <FirstSlider/>
+     
     </div>
 
   );
