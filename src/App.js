8fa767414e187280ea6cbe463123fccb8ee390c8
Адилет <110529980+AdiletURS@users.noreply.github.com>
2022-12-14 18:29:26 +0600

    Adilet Forma

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-
-import Main from './Main';
+import React from "react";
+import Main from "./Main";
+import FormSignUp from "./pages/formPages/formSignUp/FormSignUp";
+import FormLog from "./pages/formPages/formLog/FormLog";
 
 function App() {
-
   return (
     <div className="App">
-      <Main/>
+      <Main />
+      {/* <FormLog /> */}
+      {/* <FormSignUp /> */}
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,8 @@
-import React from 'react'
-import './Main.css'
-
+import React from "react";
+import "./Main.css";
 
 function Main() {
-  return (
-    <div >
-        main
-    </div>
-  )
+  return <div></div>;
 }
 
-export default Main
+export default Main;
