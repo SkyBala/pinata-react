dc120594878a3e529e75ac9d81d4d64be87236dc
unknown <erkubekt@gmail.com>
2022-12-16 17:04:46 +0600

    updated

--- src/pages/mobilkaPage/loadPage/LoadPage.js
+++ src/pages/mobilkaPage/loadPage/LoadPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import classes from './LoadPage.module.css'
+import Loading from './imgLoadPage/kOnzy.gif'
 
 function LoadPage() {
   return (
-    <div>LoadPage</div>
+    <div >
+            <div className={classes.Load}>
+                <img style={{width:'100px',height:'100px'}} src={Loading} alt=''/>
+            </div>
+    </div>
+
   )
 }
 
