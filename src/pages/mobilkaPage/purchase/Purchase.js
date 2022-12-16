348ec59f3d5690643d72a4753df75c6a8c5953a3
unknown <erkubekt@gmail.com>
2022-12-16 16:56:38 +0600

    Abykeev

--- src/pages/mobilkaPage/purchase/Purchase.js
+++ src/pages/mobilkaPage/purchase/Purchase.js
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from './Purchase.module.css'
-
+import axios from  "axios"
 
 function Purchase() {
+    const [postEmail, setPostEmail] = useState('')
+    const [postNumber, setPostNumber] = useState('')
+    const [postPassword, setPostPassword] = useState('')
+    const [check,setCheck]=useState('')
+
+
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const resp = await axios.post('server.php', {postEmail,postNumber,postPassword})
+            if(resp.status===200 || resp.status >200 && resp.status<400){
+                alert('good')
+                setCheck(true)
+            }
+            
+        } catch (error) {
+            console.log(error)
+            setCheck(false)
+
+        }
+    }
+
   return (
-      <div>Purchase</div>
+
+    <div className={classes.purchase}>
+
+      <div className="purchase">
+          <h2 className={classes.purchase_title}>Purchase</h2>
+          <div onSubmit={handleSubmit} className={classes.purchase_inputs}>
+              <div className={classes.purchase_email_address}>
+                  <p>EMAIL ADDRESS</p>
+                  <input
+                      value={postEmail}
+                      id="postEmail"
+                      onChange={(e) => setPostEmail(e.target.value)}
+                      className={classes.purchase_input}
+                      type="text"
+                  />
+              </div>
+              <div className={classes.purchase_phone_number}>
+                  <p>PHONE NUMBER</p>
+                  <input
+                      value={postNumber}
+                      id="postNumber"
+                      onChange={(e) => setPostNumber(e.target.value)}
+                      className={classes.purchase_input}
+                      type="number"
+                  />
+              </div>
+              <div className={classes.purchase_password}>
+                  <p>PASSWORD</p>
+                  <input
+                      value={postPassword}
+                      id="postPassword"
+                      onChange={(e) => setPostPassword(e.target.value)}
+                      className={classes.purchase_input}
+                      type="text"
+                  />
+              </div>
+          </div>
+          <div className={classes.purchase_btn}>
+              <button onClick={handleSubmit} className={classes.button}>Proceed</button>
+          </div>
+
+          <div className={classes.proceed_image}></div>
+          <div className={classes.proceed_image2}></div>
+          { check===false ? alert('error 404') :null}
+
+      </div>
+     
+      
+
+    </div>
   )
 }
 
 export default Purchase
+
+
+
+
+
+
+
+
+
+
+
